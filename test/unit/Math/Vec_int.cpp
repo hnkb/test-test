ad769fb3dc60bed7a03cf8f166f4de5685b3249a
Hani Kaabi <hani@miro.com>
2023-09-13 11:14:10 +0200

    Fix builds some more, enable all on Windows

--- test/unit/Math/Vec_int.cpp
+++ test/unit/Math/Vec_int.cpp
@@ -2,6 +2,7 @@
 #include <Crib/Math>
 
 #include <typeinfo>
+#include <new>
 #include <doctest/doctest.h>
 
 // using namespace Crib;
@@ -15,9 +16,13 @@ TEST_CASE("int2-basic")
 	// printf("%llu %llu\n", (uint64_t)&int2::x, (uint64_t)&int2::y);
 	CHECK(sizeof(int2) == 8);
 	CHECK(alignof(int2) == 4);
+#if !defined(__GNUC__) || defined(__clang__)
 	CHECK(typeid(int2::x) == typeid(int));
 	CHECK(typeid(int2::y) == typeid(int));
 	// CHECK(&int2::x == 0);
+#else
+	CHECK_MESSAGE(false, "GCC is not supported");
+#endif
 
 	int2 a { 5, 7 };
 	CHECK(a.x == 5);
@@ -29,12 +34,12 @@ TEST_CASE("int2-basic")
 
 	// make sure we are not zero initializing with default constructor
 	// important for performance of big vector allocation
-	auto f = new ((void*)buffer) int2;
+	auto f = new (buffer) int2;
 	CHECK(f->x == 1);
 	CHECK(f->y == 2);
 
 	// make sure zero-initialize works when using {} initializer
-	f = new ((void*)buffer) int2 {};
+	f = new (buffer) int2 {};
 	CHECK(f->x == 0);
 	CHECK(f->y == 0);
 
